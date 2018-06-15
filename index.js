a91283b0c0a2e515ad8d949918e09e4f53ffcc27
Emmanuel D Mendez <manche65@gmail.com>
2018-06-15 10:01:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 // write /ur code below!
 // console.log("Hello, World!")
 
-var name = "Susan"
-var height = "70"
+var name = "Joe"
+var height = "74"
+
+if(name === "Susan"){
+  console.log("The name is correct")
+}else {
+  console.log("check your spelling")
+}
+if(height === "70"){
+  console.log("The height is correct")
+}else{
+  console.log("enter height correctly")
+}
 //dont worry about this for now
 module.exports = {name, height}
