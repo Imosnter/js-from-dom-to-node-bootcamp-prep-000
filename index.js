d538b60a2c58b1fe0283aa46dfb5f5389d70d7ed
Emmanuel D Mendez <manche65@gmail.com>
2018-06-15 10:09:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ var name = "Joe"
 var height = "74"
 
 if(name === "Susan"){
-  console.log("The name is correct")
+  console.log("")
 }else {
   console.log("check your spelling")
 }
 if(height === "74"){
-  console.log("The height is correct")
+  console.log("")
 }else{
   console.log("enter height correctly")
 }
