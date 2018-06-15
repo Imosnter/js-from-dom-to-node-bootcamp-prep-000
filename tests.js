25874af68e01d5b3d3b5aaefbb9ca4fe3e4de44b
Emmanuel D Mendez <manche65@gmail.com>
2018-06-15 10:15:18 +0000

    automatically backed up by learn

--- tests.js
+++ tests.js
@@ -7,7 +7,7 @@ if(name.name === "Susan"){
   console.log("Expected: Susan" + ", " + "Received " + name.name )
 }
 
-if(height.height === "70"){
+if(height.height === 70){
   console.log("The height is correct")
 }else{
   console.log("Expected: 70" + ", " + "Received " + height.height)
