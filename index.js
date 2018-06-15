e0a8a971970fb9888592f69a406fa45e2b89ede9
Emmanuel D Mendez <manche65@gmail.com>
2018-06-15 07:59:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-// write your code below!
-console.log("Hello, World!")
+// write /ur code below!
+// console.log("Hello, World!")
+
+var name = "Joe"
+var height = "74"
+
+//dont worry about this for now
+module.exports = {name, height}
+
