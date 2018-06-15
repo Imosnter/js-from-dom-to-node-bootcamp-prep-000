4263a2d258020c1a60f377f344bb180e6db515f2
Emmanuel D Mendez <manche65@gmail.com>
2018-06-15 10:13:58 +0000

    automatically backed up by learn

--- tests.js
+++ tests.js
@@ -2,13 +2,13 @@ var name = require("./index.js")
 var height = require("./index.js")
 
 if(name.name === "Susan"){
-  console.log("name is correct")
+  console.log("The name is correct")
 }else{
   console.log("Expected: Susan" + ", " + "Received " + name.name )
 }
 
 if(height.height === "70"){
-  console.log("Height is correct")
+  console.log("The height is correct")
 }else{
   console.log("Expected: 70" + ", " + "Received " + height.height)
 }
