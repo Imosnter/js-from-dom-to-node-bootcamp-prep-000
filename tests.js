3550f249cf17cd3f1ea498fd484e1cb50d3af03f
Emmanuel D Mendez <manche65@gmail.com>
2018-06-15 09:14:02 +0000

    automatically backed up by learn

---
+++ tests.js
@@ -0,0 +1,11 @@
+if(name === "Susan"){
+  console.log("name is correct")
+}else{
+  console.log("Expected name to be Susan")
+}
+
+if(height === "70"){
+  console.log("Height is correct")
+}else{
+  console.log("Expected height to be 70")
+}
